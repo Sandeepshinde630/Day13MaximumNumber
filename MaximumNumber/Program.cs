793539b030f0e81abb94e1ffce63a4dbdffaf5b2
unknown <sandeepshinde630@gmail.com>
2021-08-14 11:54:04 +0530

    feat:Find maximum Integer

--- MaximumNumber/Program.cs
+++ MaximumNumber/Program.cs
@@ -7,6 +7,9 @@ namespace MaximumNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Maximum Number Problem");
+            Console.WriteLine("maximum number:");
+            int MaxNumber = TestCase.MaximumInteger(66, 22, 33);
+            Console.WriteLine(MaxNumber);
         }
     }
 }
